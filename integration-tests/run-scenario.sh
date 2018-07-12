a674098254b4b84b6dbf5fd06f535678624b46c1
Pasindu <pasinduonline@gmail.com>
2018-07-12 22:37:32 +0530

    RERE

--- integration-tests/run-scenario.sh
+++ integration-tests/run-scenario.sh
@@ -145,6 +145,7 @@ if [ "${os}" = "Windows" ]; then
   #sshpass -p "${password}" scp -q -o StrictHostKeyChecking=no ${user}@${host}:${REM_DIR}/product-apim/modules/integration/tests-integration/tests-backend/target/logs/automation.log ${DIR}
   #sshpass -p "${password}" scp -q -o StrictHostKeyChecking=no ${user}@${host}:${REM_DIR}/output.properties ${DIR}
   scp -r -i ${key_pem} -o StrictHostKeyChecking=no ubuntu@testgrid-live-dev.private.wso2.com:/home/ubuntu/surefire-reports ${DIR}
+  scp -i ${key_pem} -o StrictHostKeyChecking=no ubuntu@testgrid-live-dev.private.wso2.com:/home/ubuntu/output.properties ${DIR}
   echo "=== Reports retrieved successfully ==="
   #set -o xtrace
 else
@@ -167,6 +168,8 @@ else
   #scp -o StrictHostKeyChecking=no -r -i ${key_pem} ${user}@${host}:${REM_DIR}/product-apim/modules/integration/tests-integration/tests-backend/target/logs/automation.log ${DIR}
   #scp -o StrictHostKeyChecking=no -r -i ${key_pem} ${user}@${host}:${REM_DIR}/output.properties ${DIR}
   scp -i ${key_pem} -o StrictHostKeyChecking=no ubuntu@testgrid-live-dev.private.wso2.com:/home/ubuntu/surefire-reports ${DIR}
+  scp -i ${key_pem} -o StrictHostKeyChecking=no ubuntu@testgrid-live-dev.private.wso2.com:/home/ubuntu/output.properties ${DIR}
+
   echo "=== Reports are copied success ==="
 fi
 ##script ends
